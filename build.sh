f54465d69e440f7e1e0e316bec5b91eeec1bc94b
Vo Minh Thu <thu@hypered.io>
2015-11-21 15:35:44 +0100

    Add a 7.8.4 variant.

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 docker build \
   --no-cache \
-  -t images.reesd.com/reesd/stack-dependencies \
+  -t images.reesd.com/reesd/stack-dependencies:7.8.4 \
   images/stack-dependencies
 
 docker build \
-  -t images.reesd.com/reesd/stack \
+  -t images.reesd.com/reesd/stack:7.8.4 \
   images/stack
