88e9148712e8e338d1624e51a3e9e1402ab1377b
Vo Minh Thu <thu@hypered.io>
2017-01-16 15:08:10 +0100

    Pin some dependencies.

--- images/stack-dependencies/build.sh
+++ images/stack-dependencies/build.sh
@@ -6,6 +6,16 @@ sed -i 's/hackage.haskell.org\/packages\/archive/hackage.reesd.com\//' /home/gus
 cabal update
 
 cd /home/gusdev/stack-dependencies
+echo
+echo ------ Installing text...
 cabal install -f-integer-simple text-1.2.1.3
+echo
+echo ------ Installing QuickCheck
+# Otherwise alex or happy will install a more recent QuickCheck.
+cabal install QuickCheck-2.8.2
+echo
+echo ------ Installing alex, happy...
 cabal install alex happy
+echo
+echo ------ Installing remaing packages...
 cabal install --only-dependencies
