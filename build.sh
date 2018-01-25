09cbb6dcea73f87f52cc842b73f578ecfa604b3e
Vo Minh Thu <thu@hypered.io>
2018-01-25 15:47:47 +0100

    Use http-streams-0.8.5.3

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@
 # Base images (builders)
 #
 
-#  --no-cache \
+
+#docker build \
+#  -f images/stack-dependencies/Dockerfile-7.8.4 \
+#  -t images.reesd.com/reesd/stack-dependencies:7.8.4 \
+#  images/stack-dependencies
 
 docker build \
-  -f images/stack-dependencies/Dockerfile-7.8.4 \
-  -t images.reesd.com/reesd/stack-dependencies:7.8.4 \
-  images/stack-dependencies
-
-docker build \
+  --no-cache \
   -f images/stack-dependencies/Dockerfile-7.10.3 \
   -t images.reesd.com/reesd/stack-dependencies:lts-7.10.3 \
   images/stack-dependencies
@@ -43,6 +43,6 @@ docker build \
   images/stack
 
 
-# I guess the LTS-based one is as good as mine.
-docker tag -f images.reesd.com/reesd/stack:lts-7.8.4-reesd images.reesd.com/reesd/stack:7.8.4
-docker tag -f images.reesd.com/reesd/stack:lts-7.10.3-reesd images.reesd.com/reesd/stack:7.10.3
+# Uncomment when rebuilding.
+# docker tag -f images.reesd.com/reesd/stack:lts-7.8.4-reesd images.reesd.com/reesd/stack:7.8.4
+# docker tag -f images.reesd.com/reesd/stack:lts-7.10.3-reesd images.reesd.com/reesd/stack:7.10.3
